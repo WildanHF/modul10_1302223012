3386f7b151f1720f1e54dbdf872b7c5e6c8226ed
WildanHF <whadi611@gmail.com>
2024-05-04 08:20:44 +0700

    first commit

---
+++ mod10_1302223012/Mahasiswa.cs
@@ -0,0 +1,10 @@
+namespace mod10_1302223012
+{
+    public class Mahasiswa
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+    }
